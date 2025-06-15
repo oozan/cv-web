e1076d70246716a58fa2bf89cab472ccfebffcd3
oozan <oozayranci@gmail.com>
2025-06-15 18:45:47 +0300

    contact 3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,9 @@ export default function Footer() {
     <footer className="w-full bg-black py-6">
       <div className="text-center text-gray-500 text-sm">
         Copyright © 2023 Ozan Özayranci. All rights reserved.
+        <div className="text-center text-gray-500 text-sm">
+          +358414889262 oozayranci@gmail.com Helsinki
+        </div>
       </div>
     </footer>
   );
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,9 +10,7 @@ const Profile = () => {
           <h2>Ozan Özayranci</h2>
           <p className="text-lg text-gray-400 mb-6 w-[16rem]">
             Software Engineer 
-            +358414889262 
-            oozayranci@gmail.com 
-            Helsinki
+  
           </p>
           {/* Profile Image */}
           <div className="w-40 h-40 relative overflow-hidden shadow-md border-4 border-gray-700">
